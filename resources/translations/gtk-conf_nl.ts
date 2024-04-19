3338b328d23f5b56f35db74755ad8417ea13f43c
Heimen Stoffels <vistausss@fastmail.com>
2024-04-19 17:08:43 +0200

    Added translation using Weblate (Dutch)

---
+++ resources/translations/gtk-conf_nl.ts
@@ -0,0 +1,374 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>DialogMain</name>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="47"/>
+        <source>Themes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="55"/>
+        <source>Font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="63"/>
+        <source>Sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="71"/>
+        <source>Other</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="107"/>
+        <source>Color scheme:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="114"/>
+        <source>GTK theme:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="137"/>
+        <source>Cursor theme:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="144"/>
+        <source>Icon theme:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="154"/>
+        <source>Cursor size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="171"/>
+        <source>Prefer dark theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="194"/>
+        <source>Enable Antialiasing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="204"/>
+        <source>Enable Hinting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="230"/>
+        <source>Resolution:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="237"/>
+        <source>Default:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="244"/>
+        <source>Hinting style:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="277"/>
+        <source>RGBA order:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="287"/>
+        <source>Antialiasing:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="304"/>
+        <source>Play event sounds as feedback to user input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="320"/>
+        <source>Allow volume above 100%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="333"/>
+        <source>Play event sounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="340"/>
+        <source>Sound theme:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="379"/>
+        <source>Enable overlay scrolling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="386"/>
+        <source>Primary button warps slider</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="430"/>
+        <source>Recent files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="442"/>
+        <source>Max age:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.ui" line="480"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.cpp" line="78"/>
+        <source>Settings changed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogMain.cpp" line="79"/>
+        <source>Some settings have been changed by a different application.
+
+You can reload the new settings by pressing the Reset button, or use Save if you want to keep the current ones.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="10"/>
+        <location filename="../../src/ToolTips.cpp" line="28"/>
+        <location filename="../../src/ToolTips.cpp" line="36"/>
+        <location filename="../../src/ToolTips.cpp" line="42"/>
+        <location filename="../../src/ToolTips.cpp" line="48"/>
+        <location filename="../../src/ToolTips.cpp" line="54"/>
+        <location filename="../../src/ToolTips.cpp" line="64"/>
+        <location filename="../../src/ToolTips.cpp" line="70"/>
+        <location filename="../../src/ToolTips.cpp" line="77"/>
+        <location filename="../../src/ToolTips.cpp" line="85"/>
+        <location filename="../../src/ToolTips.cpp" line="91"/>
+        <location filename="../../src/ToolTips.cpp" line="114"/>
+        <location filename="../../src/ToolTips.cpp" line="122"/>
+        <location filename="../../src/ToolTips.cpp" line="131"/>
+        <location filename="../../src/ToolTips.cpp" line="136"/>
+        <location filename="../../src/ToolTips.cpp" line="141"/>
+        <location filename="../../src/ToolTips.cpp" line="146"/>
+        <location filename="../../src/ToolTips.cpp" line="151"/>
+        <location filename="../../src/ToolTips.cpp" line="156"/>
+        <location filename="../../src/ToolTips.cpp" line="161"/>
+        <location filename="../../src/ToolTips.cpp" line="167"/>
+        <location filename="../../src/ToolTips.cpp" line="173"/>
+        <location filename="../../src/ToolTips.cpp" line="181"/>
+        <location filename="../../src/ToolTips.cpp" line="186"/>
+        <location filename="../../src/ToolTips.cpp" line="191"/>
+        <location filename="../../src/ToolTips.cpp" line="198"/>
+        <location filename="../../src/ToolTips.cpp" line="211"/>
+        <location filename="../../src/ToolTips.cpp" line="218"/>
+        <source>Default value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="8"/>
+        <source>Whether the application prefers to use a dark theme. If a GTK theme includes a dark variant, it will be used instead of the configured theme.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="14"/>
+        <source>Name of the cursor theme to use. Use %1 to use the default theme.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="19"/>
+        <source>The size to use for cursors. %1 means to use the default size.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="24"/>
+        <source>Whether to play any event sounds at all. See the Sound Theme Specifications for more information on event sounds and sound themes. GTK itself does not support event sounds, you have to use a loadable module like the one that comes with libcanberra.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="32"/>
+        <source>Whether to play event sounds as feedback to user input. See the Sound Theme Specifications for more information on event sounds and sound themes. GTK itself does not support event sounds, you have to use a loadable module like the one that comes with libcanberra.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="40"/>
+        <source>Name of the theme to load. See GtkCssProvider for details about how GTK finds the CSS stylesheet for a theme.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="46"/>
+        <source>Name of the icon theme to use. See GtkIconTheme for details about how GTK handles icon themes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="68"/>
+        <source>Whether GTK should keep track of items inside the recently used resources list. If set to %1, the list will always be empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="74"/>
+        <source>The maximum age, in days, of the items inside the recently used resources list. Items older than this setting will be excised from the list. If set to 0, the list will always be empty; if set to -1, no item will be removed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="81"/>
+        <source>The XDG sound theme to use for event sounds. See the Sound Theme Specifications for more information on event sounds and sound themes. GTK itself does not support event sounds, you have to use a loadable module like the one that comes with libcanberra.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="89"/>
+        <source>The default font to use. GTK uses the family name and size from this string.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="129"/>
+        <source>The preferred color scheme for the user interface. Valid values are:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="135"/>
+        <source>Cursor theme name. Used only by Xservers that support the Xcursor extension.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="140"/>
+        <source>Size of the cursor used as cursor theme.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="145"/>
+        <source>Whether to play sounds on user events.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="150"/>
+        <source>Whether to play sounds on input events.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="155"/>
+        <source>Basename of the default theme used by gtk+.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="160"/>
+        <source>Icon theme to use for the panel, nautilus etc.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="165"/>
+        <source>Whether scrollbars should be overlayed as indicators. Depending on input devices in use, permanent scrollbars may still be displayed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="177"/>
+        <source>Number of days to remember recently used files for. Recently used files will be remembered for this many days. If set to 0, recent files will not be remembered; if set to -1, they will be retained indefinitely.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="195"/>
+        <source>The type of antialiasing to use when rendering fonts. Possible values are: %1 for no antialiasing, %2 for standard grayscale antialiasing, and %3 for subpixel antialiasing (LCD screens only).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="202"/>
+        <source>The type of hinting to use when rendering fonts. Possible values are: %1 for no hinting and %2 for fitting only to the Y-axis like Microsoft’s ClearType, DirectWrite and Adobe’s proprietary font rendering engine. Ignores native hinting within the font, generates hints algorithmically. Used on Ubuntu by default. Recommended. The meaning of %3 and %4 depends on the font format (.ttf, .otf, .pfa/.pfb) and the installed version of FreeType. They usually try to fit glyphs to both the X and the Y axis (except for .otf: Y-only). This can lead to distortion and/or inconsistent rendering depending on the quality of the font, the font format and the state of FreeType’s font engines.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="215"/>
+        <source>The order of subpixel elements on an LCD screen; only used when antialiasing is set to %1. Possible values are: %2 for red on left (most common), %3 for blue on left, %4 for red on top, %5 for red on bottom.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="185"/>
+        <source>The XDG sound theme to use for event sounds.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="52"/>
+        <source>Whether scrolled windows may use overlaid scrolling indicators. If this is set to %1, scrolled windows will have permanent scrollbars.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="58"/>
+        <source>If the value of this setting is %1, clicking the primary button in a &lt;b&gt;GtkRange&lt;/b&gt; trough will move the slider, and hence set the range’s value, to the point that you clicked. If it is %2, a primary click will cause the slider/value to move by the range’s page-size towards the point clicked. Whichever action you choose for the primary button, the other action will be available by holding &lt;b&gt;Shift&lt;/b&gt; and primary-clicking, or clicking the middle mouse button.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="95"/>
+        <source>Whether to antialias fonts. The values are: 0 for no, 1 for yes or %1 for the system default.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="101"/>
+        <source>Whether to enable font hinting. The values are: 0 for no, 1 for yes or %1 for the system default.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="107"/>
+        <source>The font resolution, in 1024 * dots/inch. %1 to use the default value.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="112"/>
+        <source>What degree of font hinting to use. The possible values are %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="118"/>
+        <source>The type of subpixel antialiasing to use. The possible values are %1. Note that GSK does not support subpixel antialiasing, and this setting has no effect on font rendering in GTK.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="171"/>
+        <source>Whether to remember recently used files. If %1, applications will not remember recently used files.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ToolTips.cpp" line="190"/>
+        <source>Name of the default font used by gtk+.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::DialogAbout</name>
+    <message>
+        <location filename="../../src/DialogAbout.ui" line="39"/>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogAbout.ui" line="90"/>
+        <source>Authors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogAbout.ui" line="144"/>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogAbout.cpp" line="52"/>
+        <source>Author</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/DialogAbout.cpp" line="57"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
